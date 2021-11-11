dd117930a8ed76f1c207853acef7da99f98c6cc7
Luis Diaz <ldiazn98@gmail.com>
2021-11-10 22:38:37 -0400

    Finished initial setup for the server

--- hw6/calcServer.c
+++ hw6/calcServer.c
@@ -9,15 +9,20 @@
 #include <string.h>
 #include <signal.h>
 
+// Booleans
 #define TRUE 1
 #define FALSE 0
+#define bool int
+
+// max size to read from input
+#define LINEBUFF_SIZE 1024
 
 /// Server persistent data
 struct Server
 {
 	struct Calc *calc; // calc object with stored persistent data
 	size_t port;	   // port to listen to 
-	int running;   // If the server should stope
+	bool running;   // If the server should stope
 };
 
 /// Summary:
@@ -36,7 +41,7 @@ void server_shutdown(struct Server * server);
 ///		server = Pointer to the server to check if it's running
 /// Returns:
 ///		0 if not running, anything else otherwhise
-int server_running(struct Server * server);
+bool server_running(struct Server * server);
 
 /// Summary:
 /// 	Handle interruption signal (SIGINT), so we can safely destroy the server on interruption
@@ -44,13 +49,25 @@ int server_running(struct Server * server);
 /// 	signum = Signal number to handle
 void sigint_handler(int signum);
 
+/// Summary:
+///		Main function to interact with the client in an interactive http session using the given server state, 
+///		reading input from the provided file descriptor, and writing output to the provided file descriptor
+///	Parameters
+///		server = current server state
+///		infd   = file descriptor for the file where to read the input
+///		outfd  = file descriptor for the file where to write the output
+void chat_with_client(struct Server * server, int infd, int outfd);
+
 // Server object for our application
 struct Server server = {.calc=NULL, .port = 0, .running = 0};
 
+// Logger object
+Logger * logger = NULL;
 
 int main(int argc, char **argv) {
+
 	// Initialize logging
-	Logger * logger = logger_get();	
+	logger = logger_get();	
 
 	// Set up interruption signal:
 	signal(SIGINT, sigint_handler);
@@ -85,8 +102,6 @@ int main(int argc, char **argv) {
 	// Shut down server object
 	server_shutdown(&server);
 	
-	// Destroy logger
-	logger_destroy(logger);
 	return 0;
 }
 
@@ -111,12 +126,16 @@ void server_start(struct Server * server, size_t port)
 // Shut down server
 void server_shutdown(struct Server * server)
 {
+	// Destroy server object
 	LOG_INFO("Shutting down server...\n");
 	calc_destroy(server->calc);
 	server->calc = NULL;
 	server->running = FALSE;
 
 	LOG_INFO("Server shutdown succesful\n");
+
+	// Destroy logger
+	logger_destroy(logger);
 }
 
 void sigint_handler(int signum)
@@ -124,10 +143,55 @@ void sigint_handler(int signum)
 	// Sanity check
 	assert(signum == SIGINT && "Invalid signal for sigint handler");
 
+	LOG_INFO("Interruption Signal Received (SIGINT)\n");
+
 	// Server shutdown
 	server_shutdown(&server);
 
 	// Call default signal on shutdown finished
 	signal(SIGINT, SIG_DFL);
 	raise(SIGINT);
+}
+
+void chat_with_client(struct Server *server, int infd, int outfd) {
+	rio_t in;
+	char linebuf[LINEBUFF_SIZE];
+
+	/* wrap standard input (which is file descriptor 0) */
+	rio_readinitb(&in, infd);
+
+	/*
+	 * Read lines of input, evaluate them as calculator expressions,
+	 * and (if evaluation was successful) print the result of each
+	 * expression.  Quit when "quit" command is received.
+	 */
+	bool done = FALSE;
+	while (!done) {
+		ssize_t n = rio_readlineb(&in, linebuf, LINEBUFF_SIZE);
+		if (n <= 0) {
+			/* error or end of input */
+			done = TRUE;
+		} else if (strcmp(linebuf, "quit\n") == 0 || strcmp(linebuf, "quit\r\n") == 0) {
+			/* quit command */
+			done = TRUE;
+		} else if (strcmp(linebuf, "shutdown\n") == 0 || strcmp(linebuf, "shutdown\r\n") == 0) {
+			done = TRUE;
+			// Will stop the main loop
+			server_shutdown(server);
+
+		} else {
+			/* process input line */
+			int result;
+			if (calc_eval(server->calc, linebuf, &result) == 0) {
+				/* expression couldn't be evaluated */
+				rio_writen(outfd, "Error\n", 6);
+			} else {
+				/* output result */
+				int len = snprintf(linebuf, LINEBUFF_SIZE, "%d\n", result);
+				if (len < LINEBUFF_SIZE) {
+					rio_writen(outfd, linebuf, len);
+				}
+			}
+		}
+	}
 }
