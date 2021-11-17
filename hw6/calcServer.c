b439a22d9118f6cfa67bc0f042aa6ee90039c4b8
Luis Diaz <ldiazn98@gmail.com>
2021-11-17 02:06:04 -0400

    removed memory leaks and waiting for other sessions to end

--- hw6/calcServer.c
+++ hw6/calcServer.c
@@ -23,20 +23,21 @@
 #define SERVER_ADDR INADDR_LOOPBACK // Listen to localhost
 // #define SERVER_ADDR INADDR_ANY   // Listen anything
 #define MAX_CONNECTION_QUEUE_SIZE 100
-#define MAX_SIMULT_SESSIONS 10 		// Max amount of simultaneous sessions
+#define MAX_SIMULT_SESSIONS 2 		// Max amount of simultaneous sessions
 
 /// Server persistent data
 struct Server
 {
-	struct Calc *calc; 		// calc object with stored persistent data
-	uint32_t 	port;	    // port to listen to 
-	bool  		running;    // If the server should stope
-	int 		socket_fd;  // File descriptor for the server socket
-	struct sockaddr_in address; // Address object
-	int address_len;		// Size in bytes of the address object
-	int opt;				// ???
-	pthread_t main_thread_id; // Main thread id for signals
-	pthread_mutex_t mutex; 	// Mutex required to ensure thread safety
+	struct Calc *calc; 			// calc object with stored persistent data
+	uint32_t 	port;	    	// port to listen to 
+	bool  		running;    	// If the server should stope
+	int 		socket_fd;  	// File descriptor for the server socket
+	struct sockaddr_in address; 			// Address object
+	pthread_t main_thread_id; 				// Main thread id for signals
+	pthread_mutex_t calc_mutex; 			// Mutex required to ensure thread safety with calculator operations
+	pthread_mutex_t thread_pool_mutex; 		// Mutex required to ensure thread safety with thread pool
+	pthread_t threads[MAX_SIMULT_SESSIONS];	//	Pool of threads
+	bool	  thread_destroy_queue[MAX_SIMULT_SESSIONS]; // queue of threads to be destroyed
 };
 
 /// Arguments to pass to the session thread when creating it
@@ -44,6 +45,7 @@ struct SessionArgs
 {
 	int peer_socket_fd;
 	struct Server * server;
+	size_t thread_index;
 };
 
 /// Summary:
@@ -84,13 +86,28 @@ void *chat_with_client(void * args);
 
 void server_shutdown_start(struct Server * server);
 
+/// Summary:
+/// 	Clean the destroy queue, deleting threads marked for destroy
+void server_clean_destroy_queue(struct Server *server);
+
+/// Summary 
+///		Create a thread in the internal structure of the server. Returns a status code depending if it was possible
+///	Parameters
+///		server : pointer to a server to use
+///		peer_socket_fd : file descriptor for peer socket
+void server_create_thread(struct Server * server, int peer_socket_fd);
+
+/// Summary
+///		Queue this thread to be destroyed
+void server_destroy_thread(struct Server* server, size_t thread_index);
+
 // Server object for our application
 struct Server server;
 
 // Logger object
 Logger * logger = NULL;
 
-pthread_t threads[MAX_SIMULT_SESSIONS];
+// thread object buffer
 
 /// Summary:
 ///		Use this signal handler to wake up the server in case it is halting in "accept"
@@ -113,9 +130,6 @@ int main(int argc, char **argv) {
 	sig_act.sa_flags = 0;
 	sig_act.sa_handler = sigalarm_handler;
 	sigaction(SIGALRM, &sig_act, NULL);
-
-	// Reset threads to 0
-	memset(threads, 0, sizeof(threads));
 
 	// Check arguments
 	if (argc <2)
@@ -161,12 +175,9 @@ int main(int argc, char **argv) {
 		}
 
 		LOG_TRACE("New connection established!\n");
-
 		
 		// Start an interactive session
-		struct SessionArgs args = {.peer_socket_fd = peer_socket, .server = &server};
-		pthread_t next_thread_id;
-		Pthread_create(&next_thread_id, NULL, chat_with_client, (void *) &args);
+		server_create_thread(&server, peer_socket);
 	}
 
 	// Shut down server object
@@ -196,10 +207,20 @@ void server_start(struct Server * server, size_t port)
 	server->running = TRUE;
 	server->main_thread_id = pthread_self();
 	
-	// init mutex
-	if (pthread_mutex_init(&server->mutex, NULL) != 0)
+	// Reset threads to 0
+	memset(server->threads, 0, sizeof(server->threads));
+
+	// init calc mutex
+	if (pthread_mutex_init(&server->calc_mutex, NULL) != 0)
 	{
-		LOG_ERROR("Mutex initialization failed\n");
+		LOG_ERROR("Calc mutex initialization failed\n");
+		exit(1);
+	}
+
+	// init thread pool mutex
+	if (pthread_mutex_init(&server->thread_pool_mutex, NULL) != 0)
+	{
+		LOG_ERROR("Thread pool mutex initialization failed\n");
 		exit(1);
 	}
 
@@ -212,12 +233,107 @@ void server_start(struct Server * server, size_t port)
 	// Error checking 
 	if (server->socket_fd == -1 || server->socket_fd == -1)
 		LOG_ERROR("Could not get a socket using open_listenfd. Error code: %d\n", server->socket_fd);
+}
 
+/// Summary
+/// 	Clean the destroy queue, deleting threads marked for destroy
+void server_clean_destroy_queue(struct Server *server)
+{
+	LOG_INFO("Cleaning destroy queue\n");
+
+	// iterate over the thread destroy queue
+	for (size_t i = 0; i < MAX_SIMULT_SESSIONS; i ++ )
+	{
+		if (server->thread_destroy_queue[i])
+		{
+			// the thread should be over by now
+			pthread_join(server->threads[i], NULL);
+
+			server->thread_destroy_queue[i] = FALSE;
+			server->threads[i] = 0;
+		}
+	}
+}
+
+/// Summary 
+///		Create a thread in the internal structure of the server. Returns a status code depending if it was possible
+
+///	Parameters
+///		server : pointer to a server to use
+///		args   : args for the new session if it is succesfully created
+void server_create_thread(struct Server * server, int peer_socket_fd)
+{
+	// Lock thread pool, not no one can access the thread pool
+	pthread_mutex_lock(&server->thread_pool_mutex);
+
+	// Clean pending threads 
+	server_clean_destroy_queue(server);
+
+	// to create a thread, search for an index with no thread associated
+	size_t next_thread_index = 0;
+	for(; 
+			next_thread_index < MAX_SIMULT_SESSIONS && 
+			server->threads[next_thread_index]
+		; 
+			next_thread_index ++
+		);
+
+	// if no session was available return an error status code 
+	if (next_thread_index == MAX_SIMULT_SESSIONS)
+	{
+		LOG_WARN("Could not create thread, no sessions available\n");
+		Rio_writen(peer_socket_fd, "No available sessions right now, try again later :(", 52);
+		return;
+	}
+
+	// Create thread
+	struct SessionArgs * args = malloc(sizeof(struct SessionArgs));
+	args->server = server;
+	args->peer_socket_fd = peer_socket_fd;
+	args->thread_index = next_thread_index;
+
+	LOG_INFO("Starting new session with id: %lu\n", next_thread_index);
+	Pthread_create(&server->threads[next_thread_index], NULL, chat_with_client, (void *) args);
+
+	// Unlock thread pool, no some process can access the thread pool
+	pthread_mutex_unlock(&server->thread_pool_mutex);
+}
+
+/// Summary
+///		Queue this thread to be destroyed
+void server_destroy_thread(struct Server* server, size_t thread_index)
+{
+	// Sanity check
+	assert( thread_index < MAX_SIMULT_SESSIONS && "Thread index out of range");
+
+	// Lock thread pool
+	pthread_mutex_lock(&server->thread_pool_mutex);
+	LOG_INFO("Marking thread %lu for destroy\n", thread_index);
+
+	// mark it in the destroy queue
+	server->thread_destroy_queue[thread_index] = FALSE;
+
+	// Unlock thread pool
+	pthread_mutex_unlock(&server->thread_pool_mutex);
 }
 
 // Shut down server
 void server_shutdown(struct Server * server)
 {
+	// Clean sessions marked for destroy
+	server_clean_destroy_queue(server);
+
+	// Wait pending sessions
+	for (size_t i=0; i < MAX_SIMULT_SESSIONS; i++)
+	{
+		pthread_t current_thread = server->threads[i];
+		if (current_thread)
+		{
+			pthread_join(current_thread, NULL);
+			server->threads[i] = 0;
+		}
+	}
+
 	// Destroy server object
 	LOG_INFO("Shutting down server...\n");
 	calc_destroy(server->calc);
@@ -226,7 +342,7 @@ void server_shutdown(struct Server * server)
 	Close(server->socket_fd); 
 
 	// Destroy mutex
-	pthread_mutex_destroy(&server->mutex);
+	pthread_mutex_destroy(&server->calc_mutex);
 
 	LOG_INFO("Server shutdown succesful\n");
 }
@@ -273,6 +389,7 @@ void *chat_with_client(void *args) {
 
 	infd = outfd = session_args->peer_socket_fd;
 	struct Server * server = session_args->server;
+	size_t thread_index = session_args->thread_index;
 
 	/* wrap standard input (which is file descriptor 0) */
 	rio_readinitb(&in, infd);
@@ -284,7 +401,7 @@ void *chat_with_client(void *args) {
 	 */
 	bool done = FALSE;
 	while (!done) {
-		ssize_t n = rio_readlineb(&in, linebuf, LINEBUFF_SIZE);
+		ssize_t n = Rio_readlineb(&in, linebuf, LINEBUFF_SIZE);
 		LOG_TRACE("peer said %s\n", linebuf);
 		if (n <= 0) {
 			/* error or end of input */
@@ -298,7 +415,7 @@ void *chat_with_client(void *args) {
 			done = TRUE;
 
 			// Write a farewell message
-			rio_writen(outfd, "Shutting down server, have a nice day :)\n", 42);
+			Rio_writen(outfd, "Shutting down server, have a nice day :)\n", 42);
 
 			// Issue a server shutdown
 			server_shutdown_start(server);
@@ -308,16 +425,22 @@ void *chat_with_client(void *args) {
 			int result;
 			if (calc_eval(server->calc, linebuf, &result) == FAILURE) {
 				/* expression couldn't be evaluated */
-				rio_writen(outfd, "Error\n", 6);
+				Rio_writen(outfd, "Error\n", 6);
 			} else {
 				/* output result */
 				int len = snprintf(linebuf, LINEBUFF_SIZE, "%d\n", result);
 				if (len < LINEBUFF_SIZE) {
-					rio_writen(outfd, linebuf, len);
+					Rio_writen(outfd, linebuf, len);
 				}
 			}
 		}
 	}
+
+	// Queue this thread to be destroyed
+	server_destroy_thread(server, thread_index);
+
+	// Free args for this session
+	free(session_args);
 
 	// Close connection with peer
 	close(session_args->peer_socket_fd);
@@ -327,11 +450,11 @@ void *chat_with_client(void *args) {
 /// Thread safe eval
 int server_calc_eval(struct Server * server, const char *expr, int *result)
 {
-	pthread_mutex_lock(&server->mutex);
+	pthread_mutex_lock(&server->calc_mutex);
 
 	int res = calc_eval(server->calc, expr, result);
 
-	pthread_mutex_unlock(&server->mutex);
+	pthread_mutex_unlock(&server->calc_mutex);
 
 	return res;
 }
