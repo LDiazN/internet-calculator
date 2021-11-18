fbc1457aff702a88ac0aeeb90a857233b06c7164
Luis Diaz <ldiazn98@gmail.com>
2021-11-17 20:37:03 -0400

    removed debug print

--- hw6/calcTest.c
+++ hw6/calcTest.c
@@ -137,9 +137,7 @@ void testInvalidExpr(TestObjs *objs) {
 	/* bad syntax */
 	ASSERT(0 == calc_eval(objs->calc, "4 +", &result));
 	/* undefined variable */
-	int x = calc_eval(objs->calc, "a", &result);
-	LOG_WARN("x is %d\n", x);
-	ASSERT(0 == x);
+	ASSERT(0 == calc_eval(objs->calc, "a", &result));
 	/* attempt to divide by 0 */
 	ASSERT(0 == calc_eval(objs->calc, "4 / 0", &result));
 }
